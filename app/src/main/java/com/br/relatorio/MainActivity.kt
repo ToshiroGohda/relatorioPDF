095cbecaf32f43550c77368858290f69b9ad7220
goldenwitchbeato <waltermtecco@gmail.com>
2024-12-20 20:12:26 -0300

    Uri Utils added and some other stuff

--- app/src/main/java/com/br/relatorio/MainActivity.kt
+++ app/src/main/java/com/br/relatorio/MainActivity.kt
@@ -28,6 +28,8 @@ class MainActivity : ComponentActivity() {
 
 
 
+
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
--- app/src/main/java/com/br/relatorio/Telas/TelaPrincipal.kt
+++ app/src/main/java/com/br/relatorio/Telas/TelaPrincipal.kt
@@ -1,7 +1,12 @@
 package com.br.relatorio.Telas
 
-import android.net.Uri
-import android.util.Log
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.graphics.pdf.PdfDocument
+import android.os.Environment
+import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
@@ -11,38 +16,59 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asAndroidBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
-import androidx.compose.foundation.lazy.items
+import androidx.core.content.ContextCompat
+import com.br.relatorio.components.UriUtils
+import java.io.File
+import java.io.FileOutputStream
+
+
+data class Ambiente(
+    val nome: String,
+    val imagens: List<Imagem>
+)
+
+data class Imagem(
+    val imagem: Bitmap,
+    val descricao: String
+)
+
+
 
 @Composable
 fun TelaPrincipal() {
 
-    var selectedIcon by remember {
-        mutableStateOf<List<Uri?>>(emptyList())
+    val context = LocalContext.current
+
+    var logo by remember {
+        mutableStateOf<ImageBitmap?>(null)
     }
+
 
 
     val pickMedia = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.PickVisualMedia(),
-        onResult = { uri -> selectedIcon = listOf(uri) }
+        onResult = { uri ->
+            uri?.let {
+                logo = UriUtils().uriToBitmap(context, it)?.asImageBitmap()
+            }
+        }
 
     )
 
@@ -89,30 +115,61 @@ fun TelaPrincipal() {
             Text("Logo Empresa")
         }
 
-    ImageLayoutView(selectedIcon)
-
-    }
-
-
-
-}
-
-@Composable
-fun ImageLayoutView(selectedImages: List<Uri?>) {
-    LazyRow {
-        items(selectedImages) { uri ->
-            AsyncImage(
-                model = uri,
-                contentDescription = null,
-                modifier = Modifier.fillMaxWidth(),
-                contentScale = ContentScale.Fit
-            )
+        Button(
+            onClick = {
+                logo?.let { paint(null, it, nome, end) }
+            }
+        ) {
+            Text("Fazer PDF")
         }
     }
+
+
+
 }
+
+fun paint(amb: Ambiente?, icon: ImageBitmap, name: String, end: String) {
+
+    var pdfDocument: PdfDocument = PdfDocument();
+    var title: Paint = Paint()
+    var myPageInfo: PdfDocument.PageInfo? =
+        PdfDocument.PageInfo.Builder(1120, 792, 1).create()
+    var myPage: PdfDocument.Page = pdfDocument.startPage(myPageInfo);
+
+    var canvas: Canvas = myPage.canvas;
+
+    canvas.drawBitmap(icon.asAndroidBitmap(), 54F, 40F, null);
+
+    title.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL))
+    title.textSize = 15F
+
+    canvas.drawText("Nome: $name", 209F, 100F, title)
+    canvas.drawText("Endereço: $end", 209F, 80F, title)
+
+    pdfDocument.finishPage(myPage);
+
+    val file: File = File(Environment.getExternalStorageDirectory(), "Relatorio.pdf");
+
+
+    try {
+        pdfDocument.writeTo(FileOutputStream(file))
+
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+
+    pdfDocument.close()
+
+}
+
+
+
+
 
 @Preview
 @Composable
 fun PreviewTelaPrincipal() {
     TelaPrincipal();
 }
+
+
